16fbbb04606a429fee1d755577d3e9d19557ef33
Lucas Franco <lcs.franco556@gmail.com>
2024-11-19 22:11:20 -0300

    feat: create interactivity.js

---
+++ interactivity.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const calculateRouteButton = document.getElementById('calculateRoute');
+    calculateRouteButton?.addEventListener('click', () => {
+      alert('Rota calculada com sucesso!');
+    });
+  
+  });
+  
